cecd4d19b1411b9052c0cc9ab6b41856eaaa2f53
Nico Mueller <nicomueller1991@googlemail.com>
2019-10-22 22:42:50 +0200

    Reduce complexity in motion light logic

--- interface.js
+++ interface.js
@@ -21,16 +21,18 @@ let devices = [
     {name: "lightbulb_huew_1", type: "light", room: "junk_room"},
     {name: "lightbulb_tradfriw_1", type: "light", room: "staircase"},
     {name: "lightbulb_tradfriw_2", type: "light", room: "staircase"},
+    {name: "lightbulb_tradfriw_3", type: "light", room: "staircase"},
+    {name: "lightbulb_tradfriw_4", type: "light", room: "staircase"},
 ]
 
 let rooms = [
     {name: "front_door", motion_light: {enabled: false}},
-    {name: "hallway", motion_light: {enabled: true}},
+    {name: "hallway", motion_light: {enabled: false}},
     {name: "server_room", motion_light: {enabled: false}},
-    {name: "bathroom_small", motion_light: {enabled: true}},
+    {name: "bathroom_small", motion_light: {enabled: false}},
     {name: "kitchen", motion_light: {enabled: false}},
     {name: "living_room", motion_light: {enabled: false}},
-    {name: "junk_room", motion_light: {enabled: true}},
+    {name: "junk_room", motion_light: {enabled: false}},
     {name: "garden", motion_light: {enabled: false}},
     {name: "staircase", motion_light: {enabled: true}},
     {name: "office", motion_light: {enabled: false}},
@@ -49,14 +51,28 @@ let getDevicesInRoom = roomName => devices.filter(deviceIsInRoom(roomName))
 let getDevicesOfType = deviceType => devices.filter(deviceHasType(deviceType))
 let getPropertyOfMessage = property => message => message.payload[property]
 
+let getLightsInRoom = room => getDevicesInRoom(room)
+    .filter(deviceHasType("light"))
+
+let getRoomsWithEnabledMotionLight = rooms
+    .filter(motionLightEnabled)
+    .map(room => room.name)
+
 let inRoom = roomName => message => getDevice(message.name)
     .some(deviceIsInRoom(roomName))
 
 let isDeviceType = deviceType => message => getDevice(message.name)
     .some(deviceHasType(deviceType))
 
-let occupancyDetected = message => getPropertyOfMessage("occupancy")(message)
 
+let setLight = enable => device => {
+    client.publish(config.baseTopic + "/" + device.name + "/set", '{"state": "' + (enable ? "on" : "off") + '"}', qos = 1)
+}
+
+let setLightsInRoom = room => enable => getLightsInRoom(room)
+    .forEach(setLight(enable))
+
+let occupancyDetected = message => getPropertyOfMessage("occupancy")(message)
 let mqttTopic = device => config.baseTopic + "/" + device.name
 
 let devicesStream = bacon.fromBinder(function (sink) {
@@ -73,26 +89,15 @@ let roomOccupancy = room => roomStream(room)
     .filter(occupancyDetected)
     .map(true)
 
-let roomMovementLightEnable = room => roomOccupancy(room)
+let roomMovementLightTrigger = room => roomOccupancy(room)
     .flatMapLatest(
         () => bacon.once(true).merge(bacon.later(90000, false))
     )
 
-rooms
-    .filter(motionLightEnabled)
-    .forEach(
-        room => {
-            roomMovementLightEnable(room).onValue(
-                enable => {
-                    getDevicesInRoom(room)
-                        .filter(deviceHasType("light"))
-                        .forEach(
-                            device => client.publish(config.baseTopic + "/" + device.name + "/set", '{"state": "' + enable ? "on" : "off" + '"}')
-                        )
-                }
-            )
-
-        }
-    )
+getRoomsWithEnabledMotionLight.forEach(room => {
+        roomMovementLightTrigger(room)
+            .onValue(setLightsInRoom(room))
+    }
+)
 
 client.subscribe(devices.map(mqttTopic))
