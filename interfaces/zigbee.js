b91acebdc11f7998d009cbef233422efc9b07c3c
Nico Mueller <nicomueller1991@googlemail.com>
2019-11-12 18:06:03 +0100

    Wip

--- interfaces/zigbee.js
+++ interfaces/zigbee.js
@@ -10,24 +10,25 @@ const prependBaseTopic = R.concat(baseTopic + "/");
 const removeBaseTopic = R.replace(baseTopic + "/", "");
 const deviceTopic = R.compose(prependBaseTopic, getName);
 
-let isLogMessage = R.compose(R.map(), getName);
+const topicLense = R.lensProp("topic");
+const payloadLense = R.lensProp("payload");
 
-const mqttStream = bacon.fromBinder(function (sink) {
-    client.on('message', function (topic, message) {
-        sink([topic, message])
-    })
-});
-
-mqttStream
-    .map(removeBaseTopic)
-    .
-
-const ZigbeeLogStream = mqttStream
-    .filter(isLogMessage);
-
-const ZigbeeDevicesStream = mqttStream
-    .filter(!isLogMessage);
+let isLogTopic = x.startsWith("bridge/log");
 
 const client = mqtt.connect(address);
 client.subscribe(Devices.devices.map(deviceTopic));
 client.subscribe(baseTopic + "/bridge/log");
+
+const rawStream = bacon.fromBinder(function (sink) {
+    client.on('message', function (topic, message) {
+        sink({topic: topic, payload: message})
+    })
+});
+
+rawStream
+    .map(R.over(topicLense, removeBaseTopic))
+    .map(R.over(payloadLense, R.toString))
+    .map(R.over(payloadLense, JSON.parse))
+
+const deviceStream = rawStream
+    .filter(R.over(topicLense), )
