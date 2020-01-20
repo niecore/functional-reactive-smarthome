378f0c83f038f8c3c1ba06eb6b5da020b9dbc923
niecore <nicomueller1991@googlemail.com>
2020-01-20 21:53:26 +0100

    Document scene file

--- model/scenes.js
+++ model/scenes.js
@@ -3,23 +3,25 @@ const Scenes = require('../config/scenes.json');
 const Routes = require('../router');
 const Devices = require('../model/devices');
 
-// knownRooms
+// knownScenes
 const knownScenes = Scenes.scenes;
 
-// getDeviceByName :: String => Device | undefined
+// getSceneByName :: String => Device | undefined
 const getSceneByName = R.prop(R.__, knownScenes);
 
-
+// isMessageFromRemoteSensor :: Msg => Boolean
 const isMessageFromRemoteSensor = R.pipe(
     R.view(Routes.inputNameLens),
     Devices.deviceHasType("remote")
 );
 
+// isToggleAction :: Msg => Boolean
 const isToggleAction = R.pipe(
     R.view(Routes.inputDataLens),
     R.propEq("action", "toggle")
 );
 
+// isSceneSelect :: Msg => Boolean
 const isSceneSelect = R.pipe(
     R.view(Routes.inputDataLens),
     R.anyPass(
@@ -28,23 +30,20 @@ const isSceneSelect = R.pipe(
     )
 );
 
-const groupObjBy = R.curry(R.pipe(
-    // Call groupBy with the object as pairs, passing only the value to the key function
-    R.useWith(R.groupBy, [R.useWith(R.__, [R.last]), R.toPairs]),
-    R.map(R.fromPairs)
-));
-
+// isSceneSelect :: Scene => Msg => Boolean
 const sceneIsActive = scene => input => {
   return R.reduce(R.and, true)(R.values(R.mapObjIndexed(
       (num, key, obj) => R.whereEq(num)(R.propOr({},key)(input))
   )(scene)));
 };
 
-const getSceneFromAction = R.pipe(
+// getSceneFromMsg :: Msg => [Scene] | undefined
+const getSceneFromMsg = R.pipe(
     R.view(Routes.inputNameLens),
     getSceneByName
 );
 
+// getSelectedScene :: Msg => Scene
 const getSelectedScene = R.pipe(
     R.ifElse(
         R.pipe(
@@ -52,32 +51,35 @@ const getSelectedScene = R.pipe(
             R.propEq("action", "arrow_left_click"),
         ),
         R.pipe(
-            getSceneFromAction,
+            getSceneFromMsg,
             R.nth(2)
         ),
         R.pipe(
-            getSceneFromAction,
+            getSceneFromMsg,
             R.nth(3)
         )
     )
 );
 
+// toogleDefaultScene :: Msg => Scene
 const toogleDefaultScene = input => {
-    const scene_off = getSceneFromAction(input)[0];
-    const scene_on = getSceneFromAction(input)[1];
+    const scene_off = getSceneFromMsg(input)[0];
+    const scene_on = getSceneFromMsg(input)[1];
     const off = sceneIsActive(scene_off)(R.view(Routes.stateLens,input));
     return off ? scene_on: scene_off;
 };
 
+// Automation for toggle button
 const toggleAction = Routes.input
     .filter(isMessageFromRemoteSensor)
     .filter(isToggleAction)
     .map(toogleDefaultScene);
 
+// Automation for scene select button
 const sceneAction = Routes.input
     .filter(isMessageFromRemoteSensor)
     .filter(isSceneSelect)
-    .map(getSelectedScene)
+    .map(getSelectedScene);
 
 
 Routes.output.plug(toggleAction);
