68b887ca9fc057451f19532c18a63ffa93f8e420
niecore <nicomueller1991@googlemail.com>
2020-02-02 22:45:03 +0100

    Router filters messages by device and group

--- router.js
+++ router.js
@@ -1,25 +1,40 @@
 const Bacon = require("baconjs");
-const RA = require('ramda-adjunct');
 const R = require('ramda');
 const Zigbee = require('./interfaces/zigbee');
 const Shelly = require('./interfaces/shelly');
 const Devices = require("./model/devices");
 const Util = require('./model/util');
+const Groups = require("./model/groups");
+
+const filterMsgByDeviceInterface = interfaceType => R.pipe(
+    Util.convertToArray,
+    R.filter(input => Devices.deviceHasInterface(interfaceType)(input.key)),
+    Util.convertFromArray
+);
+
+const filterMsgIsGroup = R.pipe(
+    Util.convertToArray,
+    R.filter(input => Groups.isKnownGroup(input.key)),
+    Util.convertFromArray
+);
+
+const filterMsgIsDevice = R.pipe(
+    Util.convertToArray,
+    R.filter(input => !Groups.isKnownGroup(input.key)),
+    Util.convertFromArray
+);
 
 const output = new Bacon.Bus();
-
 const update = Bacon.mergeAll(Zigbee.deviceInputStream, Shelly.deviceInputStream);
 const state = update.scan({}, R.mergeDeepRight);
 const input = state.zip(update, (state, input) => [input, state]);
 
-const filterMsgByDeviceInterface = interface => R.pipe(
-    Util.convertToArray,
-    R.filter(input => Devices.deviceHasInterface(interface)(input.key)),
-    Util.convertFromArray
-);
+const devices = output.map(filterMsgIsDevice);
+const groups = output.map(filterMsgIsGroup);
 
-Zigbee.deviceOutputStream.plug(output.map(R.filter(filterMsgByDeviceInterface("zigbee"))));
-Shelly.deviceOutputStream.plug(output.map(R.filter(filterMsgByDeviceInterface("shelly"))));
+Zigbee.deviceOutputStream.plug(devices.map(filterMsgByDeviceInterface("zigbee")));
+Zigbee.deviceOutputStream.plug(groups); // currently only zigbee is handleing groups
+Shelly.deviceOutputStream.plug(devices.map(filterMsgByDeviceInterface("shelly")));
 
 module.exports = {
     output,
