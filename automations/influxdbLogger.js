372a92aee134bd00f029d792bfa48ef1ab449731
niecore <nicomueller1991@googlemail.com>
2020-02-11 00:36:36 +0100

    Influxdb is now only pushing valid values

--- automations/influxdbLogger.js
+++ automations/influxdbLogger.js
@@ -4,6 +4,7 @@ const Routes = require('../router');
 const Lenses = require('../lenses');
 const Devices = require('../model/devices');
 const Rooms = require('../model/rooms');
+const Util = require('../model/util');
 const Interfaces = require('../config/interfaces.json');
 
 const influx = new Influx.InfluxDB({
@@ -11,12 +12,14 @@ const influx = new Influx.InfluxDB({
     database: Interfaces.influxdb.db
 });
 
-Routes.input.onValue( value => {
+Routes.input.onValue(value => {
 
     const device = R.view(Lenses.inputNameLens)(value);
     const type = Devices.getDeviceByName(device).type;
     const room = Rooms.getRoomOfDevice(device);
     const data = R.view(Lenses.inputDataLens)(value);
+
+    const isValidType = v => (R.type(v) === 'Number' || R.type(v) === 'String' || R.type === 'Boolean');
 
     influx.writePoints([
         {
@@ -26,7 +29,7 @@ Routes.input.onValue( value => {
                 type: type,
                 room: room,
             },
-            fields: data
+            fields: R.filter(isValidType, data)
         }
     ]).catch(error => {
         console.error(`Error saving data to InfluxDB! ${err.stack}`)
