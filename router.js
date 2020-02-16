bf566396ebc0a6c1044690fd101e6c73846b69a2
niecore <nicomueller1991@googlemail.com>
2020-02-16 21:30:49 +0100

    Integrate miscale via bt-mqtt-bridge

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const R = require('ramda');
 const Zigbee = require('./interfaces/zigbee');
 const Shelly = require('./interfaces/shelly');
 const EasyControl = require('./interfaces/easy_control');
+const XiaomiScale = require('./interfaces/xiaomi_scale');
 const Devices = require("./model/devices");
 const Util = require('./model/util');
 const Groups = require("./model/groups");
@@ -31,6 +32,7 @@ const update = new Bacon.Bus();
 
 update.plug(Zigbee.deviceInputStream);
 update.plug(Shelly.deviceInputStream);
+update.plug(XiaomiScale.deviceInputStream);
 
 EasyControl.deviceInputStream.then(function (stream) {
     update.plug(stream)
