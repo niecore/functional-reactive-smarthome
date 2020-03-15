78abe2c3ed4722bcd693ef732b71eaf7fad13c1f
niecore <nicomueller1991@googlemail.com>
2020-03-15 20:45:02 +0100

    Implement presence logic layer

--- automations/motionLight.js
+++ automations/motionLight.js
@@ -1,49 +1,21 @@
 const R = require("ramda");
-const Automations = require('../config/automations.json');
-const Devices = require("../model/devices");
-const Rooms = require("../model/rooms");
-const Lenses = require('../lenses');
+const Bacon = require("baconjs");
+const Presence = require('../model/presence');
 const Light = require('../model/light');
 const Routes = require('../router');
-
-// occupancyLens :: Lens
-const occupancyLens = R.compose(Lenses.inputDataLens, R.lensPath(["occupancy"]));
-
-// occupancyLens :: Msg => Boolean
-const movementDetected = R.view(occupancyLens);
-
-// isMessageFromMotionSensor :: Msg => Boolean
-const isMessageFromMotionSensor = R.pipe(
-    R.view(Lenses.inputNameLens),
-    Devices.deviceHasType("motion_sensor")
-);
+const Automations = require("../config/automations");
 
 // isMessageFromRoomWithMotionLight :: Msg => Boolean
-const isMessageFromRoomWithMotionLight = R.pipe(
-    R.view(Lenses.inputNameLens),
-    Rooms.getRoomOfDevice,
+const isPresenceFromRoomWithMotionLight = R.pipe(
+    Presence.getRoomOfPresence,
     R.includes(R.__, R.keys(Automations.automations.motionLight.rooms))
 );
 
-const getRoomOfMessage = R.pipe(
-    R.view(Lenses.inputNameLens),
-    Rooms.getRoomOfDevice
-);
-
 const motionLight = Routes.input
-    .filter(isMessageFromMotionSensor)
-    .filter(isMessageFromRoomWithMotionLight)
-    .filter(movementDetected)
-    .groupBy(getRoomOfMessage)
+    .filter(Presence.isMessageFromPresence)
+    .groupBy(Presence.getRoomOfPresence)
     .flatMap(function(groupedStream) {
         return groupedStream.flatMapLatest(Light.setAdaptiveBrightnessInRoom)
     });
 
 Routes.output.plug(motionLight);
-
-module.exports = {
-    isMessageFromMotionSensor,
-    isMessageFromRoomWithMotionLight,
-    movementDetected,
-};
-
