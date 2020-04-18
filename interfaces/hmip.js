c022accaf136602c867f0ab1fc99f1251b876bef
niecore <nicomueller1991@googlemail.com>
2020-04-19 00:53:45 +0200

    Hmip adjustments

--- interfaces/hmip.js
+++ interfaces/hmip.js
@@ -4,8 +4,8 @@ const xmlrpc = require('homematic-xmlrpc');
 const Interfaces = require("../config/interfaces");
 const Devices = require("../model/devices");
 
-const server = xmlrpc.createServer({host: Interfaces.hmip.client_address, port: 2010});
-const init_url = 'http://' + Interfaces.hmip.client_address + ':' + 2010;
+const server = xmlrpc.createServer({host: "0.0.0.0", port: 2011});
+const init_url = 'http://' + Interfaces.hmip.client_address + ':' + 2011;
 const client = xmlrpc.createClient({
     host: Interfaces.hmip.server_address,
     port: 2010,
@@ -46,15 +46,33 @@ const isHmipDevice = R.propEq("interface", "hmip");
 
 const hmipDevices = R.pickBy(isHmipDevice, Devices.knownDevices);
 
-const isFromKnownHmipDevice = params => {
-    return R.includes(
-        params[1].split(":")[0],
-        R.values(R.map(device => device.address, hmipDevices))
-    )
-};
+const supportedDeviceTypes = ["etrv"];
 
-const isTemperatureValue = params => {
-    return R.includes(params[2], ["ACTUAL_TEMPERATURE", "SET_POINT_TEMPERATURE"])
+const transformToFrsObject = x => R.objOf(x[0], R.objOf(x[1], x[2]));
+
+const getKnownHmipAddresses = R.values(R.map(device => device.address, hmipDevices));
+
+const removeChannelFromAddress = x => x.split(":")[0];
+
+const isSupportedEtrvValue = key => R.includes(key, ["ACTUAL_TEMPERATURE", "SET_POINT_TEMPERATURE"]);
+
+const etrVtransformValueKey = R.prop(R.__, {
+    ACTUAL_TEMPERATURE: "temperature",
+    SET_POINT_TEMPERATURE: "setpoint"
+});
+
+const isFromKnownHmipDevice = R.pipe(
+    R.nth(1),
+    removeChannelFromAddress,
+    R.includes(R.__, getKnownHmipAddresses)
+);
+
+const fromHmipData = params => {
+    if (params[3] === "etrv") {
+        if (isSupportedEtrvValue(params[1])) {
+            return [params[0], etrVtransformValueKey(params[1]), params[2]]
+        }
+    }
 };
 
 const getDeviceNameFromAddress = address => {
@@ -65,15 +83,19 @@ const getDeviceNameFromAddress = address => {
     )(hmipDevices);
 };
 
-const transformValueKey = R.prop(R.__, {
-    ACTUAL_TEMPERATURE: "temperature",
-    SET_POINT_TEMPERATURE: "setpoint"
-});
+const getDeviceType = R.pipe(
+    Devices.getDeviceByName,
+    R.prop("type")
+);
 
 const deviceInputStream = Kefir.fromEvents(server, 'event', (_, params, __) => params)
     .filter(isFromKnownHmipDevice)
-    .filter(isTemperatureValue)
-    .map(x => R.objOf(getDeviceNameFromAddress(x[1]), R.objOf(transformValueKey(x[2]), x[3])));
+    .map(x => [getDeviceNameFromAddress(x[1]), x[2], x[3]])
+    .map(x => [x[0], x[1], x[2], getDeviceType(x[0])])
+    .filter(x => R.includes(x[3], supportedDeviceTypes))
+    .map(fromHmipData)
+    .filter(R.pipe(R.isNil, R.not))
+    .map(transformToFrsObject);
 
 module.exports = {
     deviceInputStream,
