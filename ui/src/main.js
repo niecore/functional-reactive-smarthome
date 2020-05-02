8bd10701421f6648baf3ec44502801cf6bc92ce9
niecore <nicomueller1991@googlemail.com>
2020-05-02 16:49:20 +0200

    Add pwa

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,16 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueSocketIO from "vue-socket.io"
+import VueSocketIO from "vue-socket.io";
 import router from "./router";
-import './../node_modules/bulma/css/bulma.css';
+import "./../node_modules/bulma/css/bulma.css";
 
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "./registerServiceWorker";
 
-Vue.use(new VueSocketIO({
+Vue.use(
+  new VueSocketIO({
     debug: true,
-    connection: "http://192.168.0.178:9090",
-}));
+    connection: "http://192.168.0.178:9090"
+  })
+);
 
 Vue.config.productionTip = false;
 
