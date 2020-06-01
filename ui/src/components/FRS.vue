4e6af05496a7f24d42ee750c5bed43fc71b77304
Nico Mueller <nicomueller1991@googlemail.com>
2020-06-01 18:01:29 +0200

    Filter for rooms on ui

--- ui/src/components/FRS.vue
+++ ui/src/components/FRS.vue
@@ -1,67 +1,87 @@
 <template>
-  <div>
-    <div class="container">
-      <div v-for="(data, device) in lights" :key="device">
-        <Device :state="data" :name="device"></Device>
-      </div>
+    <div class="columns">
+        <div class="column auto">
+
+            <p class="menu-label"><b>Room Filter</b></p>
+
+            <div class="columns">
+                <div class="column has-text-left">
+                    <span>living_room</span>
+                </div>
+                <div class="column has-text-righ">
+                    <div class="field">
+                        <input :id="device + '_state_btn'" type="checkbox" class="switch"/>
+                        <label :for="device + '_state_btn'"></label>
+                    </div>
+                </div>
+            </div>ls
+
+
+        </div>
+        <div class="column is-three-quarters">
+            <div class="container">
+                <div v-for="(data, device) in lights" :key="device">
+                    <Device :state="data" :name="device"></Device>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Device from "../components/Device";
+    import Device from "../components/Device";
 
-const R = require("ramda");
-const Devices = require("./../../../model/devices");
-const Rooms = require("./../../../model/rooms");
+    const R = require("ramda");
+    const Devices = require("./../../../model/devices");
+    const Rooms = require("./../../../model/rooms");
 
-export default {
-  name: "FRS",
+    export default {
+        name: "FRS",
 
-  components: {
-    Device
-  },
+        components: {
+            Device
+        },
 
-  data() {
-    return {
-      input: {},
-      state: {},
-      devices: {}
+        data() {
+            return {
+                input: {},
+                state: {},
+                devices: {}
+            };
+        },
+        computed: {
+            lights() {
+                return R.filter(R.propEq("type", "light"))(this.devices);
+            }
+        },
+
+        sockets: {
+            frs(data) {
+                data = JSON.parse(data);
+                this.input = data[0];
+                this.state = R.mergeDeepRight(data[1], data[0]);
+
+                this.devices = R.pipe(
+                    R.mapObjIndexed((value, key) =>
+                        R.assoc("type", Devices.getTypeOfDevice(key), value)
+                    ),
+                    R.mapObjIndexed((value, key) =>
+                        R.assoc("room", Rooms.getRoomOfDevice(key), value)
+                    )
+                )(this.state);
+            }
+        }
     };
-  },
-  computed: {
-    lights() {
-      return R.filter(R.propEq("type", "light"))(this.devices);
-    }
-  },
-
-  sockets: {
-    frs(data) {
-      data = JSON.parse(data);
-      this.input = data[0];
-      this.state = R.mergeDeepRight(data[1], data[0]);
-
-      this.devices = R.pipe(
-        R.mapObjIndexed((value, key) =>
-          R.assoc("type", Devices.getTypeOfDevice(key), value)
-        ),
-        R.mapObjIndexed((value, key) =>
-          R.assoc("room", Rooms.getRoomOfDevice(key), value)
-        )
-      )(this.state);
-    }
-  }
-};
 </script>
 
 <style scoped>
-.container {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-gap: 0.5em;
-}
+    .container {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 0.5em;
+    }
 
-.card {
-  height: max-content;
-}
+    .card {
+        height: max-content;
+    }
 </style>
